d90c80074367a537b208360732a198e91fbf6050
Charles DeLeo <flatironschool@Flatirons-MacBook-Air.local>
2019-09-27 12:27:30 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -14,5 +14,7 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
+sql = "CREATE TABLE IF NOT EXISTS artists(id INTEGER PRIMARY KEY, name TEXT, genre TEXT, age INTEGER, hometown TEXT)"
+ActiveRecord::Base.connection.execute(sql)
 
 require_relative "../artist.rb"
